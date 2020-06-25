fb9a8a3b07219902579b8ab8f324bc4d0910031e
Zoe <zoe.sylvester.xo@gmail.com>
2020-06-25 12:06:28 -0400

    I think I have finally managed to import the Ice after my third attempt!

---
+++ Ice3/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Ice3
+{
+    partial class frmIce3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(26, 13);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(424, 81);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // frmIce3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 123);
+            this.Controls.Add(this.txtOutput);
+            this.Name = "frmIce3";
+            this.Text = "Ice 03";
+            this.Load += new System.EventHandler(this.frmIce3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Ice3/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ice3
+{
+    public partial class frmIce3 : Form
+    {
+        public frmIce3()
+        {
+            InitializeComponent();
+        }
+
+        private void frmIce3_Load(object sender, EventArgs e)
+        {
+            txtOutput.Text = "Ice3 Version 1.0";
+        }
+    }
+}
